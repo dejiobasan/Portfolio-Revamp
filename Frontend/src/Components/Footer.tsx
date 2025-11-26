0610964b74725c81b2511575ab3caf2eaeb18278
Ayodeji Obasan <dejiobasan02@gmail.com>
2025-11-26 13:45:49 +0100

    feat: added dark mode.

--- Frontend/src/Components/Footer.tsx
+++ Frontend/src/Components/Footer.tsx
@@ -5,24 +5,24 @@ import  myImage  from "../Images/myImage.png"
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-inherit text-black m-10 py-6">
+    <footer className="bg-inherit text-black dark:text-white m-10 py-6 transition-colors duration-300">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center text-center md:text-left">
         <div className="flex items-center">
           <img src={myImage} alt="Logo" className="h-8 mr-2" />
           <span className="text-lg font-bold">Deji&#8482;</span>
         </div>
         <div className="flex space-x-4">
-          <Link to="https://github.com/dejiobasan" className="text-black hover:text-gray-500"><Github/></Link>
-          <Link to="https://www.linkedin.com/in/ayodeji-obasan-gmnse-9096151b9/" className="text-black hover:text-gray-500"><Linkedin/></Link>
-          <Link to="https://www.instagram.com/deji_obs/" className="text-black hover:text-gray-500"><Instagram/></Link>
+          <Link to="https://github.com/dejiobasan" className="text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><Github/></Link>
+          <Link to="https://www.linkedin.com/in/ayodeji-obasan-gmnse-9096151b9/" className="text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><Linkedin/></Link>
+          <Link to="https://www.instagram.com/deji_obs/" className="text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-300"><Instagram/></Link>
         </div>
         <div className="flex space-x-6 mt-4 md:mt-0">
-          <a href="#about" className="text-black hover:text-gray-500">About</a>
-          <a href="#projects" className="text-black hover:text-gray-500">Projects</a>
-          <a href="#skills" className="text-black hover:text-gray-500">Tools</a>
+          <a href="#about" className="text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-300">About</a>
+          <a href="#projects" className="text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-300">Projects</a>
+          <a href="#skills" className="text-black hover:text-gray-500 dark:text-white dark:hover:text-gray-300">Tools</a>
         </div>
       </div>
-      <hr className="border-black my-4" />
+      <hr className="border-black dark:border-white my-4" />
       <div className="text-center mt-4 text-sm">© 2025 Deji. All rights reserved.</div>
     </footer>
   );
--- Frontend/src/Components/Navbar.tsx
+++ Frontend/src/Components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import  myImage  from "../Images/myImage.png"
 import { useState } from "react";
+import ThemeToggle from "./ThemeToggle";
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-blue-800 text-white p-4 fixed w-full top-0 shadow-md z-50">
+    <nav className="bg-blue-800 dark:bg-gray-900 text-white p-4 fixed w-full top-0 shadow-md z-50 transition-colors duration-300">
       <div className="container mx-auto flex justify-between items-center">
       <div className="flex items-center">
           <img src={myImage} alt="Logo" className="h-8 w-8 mr-2" />
@@ -27,11 +28,12 @@ const Navbar: React.FC = () => {
         <button className="md:hidden" onClick={toggleMenu}>
           ☰
         </button>
-        <ul className={`md:flex space-x-6 absolute md:static bg-blue-800 w-full left-0 md:w-auto md:flex-row flex-col items-center transition-all duration-300 ease-in-out ${isOpen ? "top-12" : "-top-60"}`}>
+        <ul className={`md:flex space-x-6 absolute md:static bg-blue-800 dark:bg-gray-900 w-full left-0 md:w-auto md:flex-row flex-col items-center transition-all duration-300 ease-in-out ${isOpen ? "top-12" : "-top-60"}`}>
           <li className="cursor-pointer p-2" onClick={() => scrollToSection("about")}>About</li>
           <li className="cursor-pointer p-2" onClick={() => scrollToSection("skills")}>Skills</li>
           <li className="cursor-pointer p-2" onClick={() => scrollToSection("projects")}>Projects</li>
           <li className="cursor-pointer p-2" onClick={() => scrollToSection("contact")}>Contact</li>
+          <li className="p-2"><ThemeToggle /></li>
         </ul>
       </div>
     </nav>
